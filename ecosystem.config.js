0f5d7f653c6235c9d97d9ded2f0541fa5cbeebfb
Gabriel Alberto <gabrielalberto@MacBook-Air-de-Gabriel.local>
2026-02-09 18:43:19 -0300

    fix: auth trust

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,19 @@ module.exports = {
   apps: [
     {
       name: 'madua-platform',
-      // Usar o servidor standalone do Next.js para melhor performance
       script: '.next/standalone/server.js',
       instances: 1,
       autorestart: true,
-      watch: false, // Em produção, não queremos reiniciar a cada ficheiro alterado
-      max_memory_restart: '1G', // Reinicia a app se exceder 1GB de memória (segurança para VPS)
+      watch: false,
+      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
         PORT: 3001,
-        // Importante: definir o hostname para o servidor escutar
         HOSTNAME: '0.0.0.0',
+        // --- ADICIONE ESTAS LINHAS ABAIXO ---
+        AUTH_TRUST_HOST: 'true',
+        NEXTAUTH_URL: 'https://madua.com.br',
+        // ------------------------------------
       },
       error_file: './logs/pm2-error.log',
       out_file: './logs/pm2-out.log',
